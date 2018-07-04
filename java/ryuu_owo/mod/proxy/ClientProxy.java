3fca7acf3f618c49ff19635aa7b40961dc859484
RyuuOwO <40838935+RyuuOwO@users.noreply.github.com>
2018-07-04 21:34:05 +0200

    Add files via upload

---
+++ java/ryuu_owo/mod/proxy/ClientProxy.java
@@ -0,0 +1,15 @@
+package ryuu_owo.mod.proxy;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import ryuu_owo.mod.util.Reference;
+
+public class ClientProxy extends CommonProxy {
+	
+	@Override
+	public void registerItemRenderer(Item item, int meta, String id) {
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+     }	
+}
---
+++ java/ryuu_owo/mod/util/IHasModel.java
@@ -0,0 +1,5 @@
+package ryuu_owo.mod.util;
+
+public interface IHasModel {
+	public void registerModels();
+}
